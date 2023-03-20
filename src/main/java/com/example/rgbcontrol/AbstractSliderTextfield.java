627d3a96970412e8f3cd7be68c8fe23e9f0e9bdb
EmmaV <emmatje.vandewalle@gmail.com>
2023-03-20 21:59:02 +0100

    extra: temp for converter cleanup (must be fixed later)

--- src/main/java/com/example/rgbcontrol/AbstractSliderTextfield.java
+++ src/main/java/com/example/rgbcontrol/AbstractSliderTextfield.java
@@ -16,51 +16,9 @@ abstract public class AbstractSliderTextfield {
             value.setValue(newV.intValue());
         });
 
-//        score.textProperty().addListener((obs, oldV, newV) -> {
-//            if (newV.isEmpty()) return;
-//            try {
-//                int number = Integer.parseInt(newV);
-//                if (number < minVal || number > maxVal) {
-//                    score.setText(oldV);
-////                    value.setValue(Integer.parseInt(oldV));
-//                    return;
-//                }
-//                score.setText(newV);
-////                value.setValue(number);
-//            } catch (NumberFormatException e) {
-//                score.setText(oldV);
-////                value.setValue(Integer.parseInt(oldV));
-//            }
-//        });
-
         score.setTextFormatter(new TextFormatter<Number>(new NumberStringConverter(new DecimalFormat())));
 
-//        score.setTextFormatter(new TextFormatter<>(c -> {
-//            if (c.isContentChange()) {
-//                if (c.getControlNewText().length() == 0) {
-//                    System.out.println("in if");
-//                    return c;
-//                }
-//
-//                try {
-//                    Integer.parseInt(c.getControlNewText());
-//                    if (Integer.parseInt(c.getControlNewText()) < minVal || Integer.parseInt(c.getControlNewText()) > maxVal) {
-//                        System.out.println("in parse");
-//                        return null;
-//                    }
-//                    return c;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("in catch");
-//                    e.getMessage();
-//                }
-//
-//            }
-//            return null;
-//        }));
-
         Bindings.bindBidirectional(score.textProperty(), value.valueProperty(), new NumberStringConverter());
-
-//        score.textProperty().bind(value.valueProperty().asString("%.0f"));
     }
 
 }
